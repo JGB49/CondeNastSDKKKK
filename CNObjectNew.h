c0dc5c7dbd971e417766c49797f09dbfa8fd8591
guangbi.jin <guangbi.jin@condenast.com.cn>
2016-09-29 14:52:35 +0800

    init CCCCCSDSDS

---
+++ CNObjectNew.h
@@ -0,0 +1,15 @@
+//
+//  CNObjectNew.h
+//  ProjectM
+//
+//  Created by jinguangbi on 16/9/29.
+//  Copyright © 2016年 jinguangbi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CNObjectNew : NSObject
+
+-(NSString *)returnUserName;
+
+@end
---
+++ CNObjectNew.m
@@ -0,0 +1,21 @@
+//
+//  CNObjectNew.m
+//  ProjectM
+//
+//  Created by jinguangbi on 16/9/29.
+//  Copyright © 2016年 jinguangbi. All rights reserved.
+//
+
+#import "CNObjectNew.h"
+
+@implementation CNObjectNew
+
+
+
+-(NSString *)returnUserName {
+
+    return @"Hello";
+    
+}
+
+@end
